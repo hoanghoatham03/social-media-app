1fec0958a3cb5f14483055d73f432a660e8f6b16
nthungse03 <nthungse03@gmail.com>
2025-02-19 19:08:06 +0700

    init fe project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
